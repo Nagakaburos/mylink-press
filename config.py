bf9f270c959338dc5736eaa8d5a82bf80f4326a6
Nagakaburos <wansanice@proton.me>
2025-05-09 12:29:46 -0300

    Primeiro commit

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
